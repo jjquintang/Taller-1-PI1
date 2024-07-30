11a69cde5b058604a9555919a7e7a62f12c2f056
Jquintanag <jjquintang@eafit.edu.co>
2024-07-30 07:34:57 -0500

    “cambios”

--- movie/views.py
+++ movie/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Greg Lim'})
+    return render(request, 'home.html', {'name': 'Juan José Quintana'})
