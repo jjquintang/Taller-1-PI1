b04acf50566d9944708da50434e57e3bc45ef260
Jquintanag <jjquintang@eafit.edu.co>
2024-07-31 22:40:41 -0500

    visualizacion de peliculas con estilo

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+from .models import movie
 # Create your views here.
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Juan José Quintana'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = movie.objects.filter(title__icontains = searchTerm)
+    else:
+        movies = movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, "movies" : movies})
 
 def about(request):
     return render(request, 'about.html')
